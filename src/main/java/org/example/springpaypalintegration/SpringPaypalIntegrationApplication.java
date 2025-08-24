8c3c2595d317862093830c979b69064b1fb540eb
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2025-08-24 15:18:49 +0300

    added some html pages, and renew createPayment method

---
+++ src/main/java/org/example/springpaypalintegration/SpringPaypalIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.example.springpaypalintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPaypalIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springpaypalintegration/SpringPaypalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springpaypalintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
